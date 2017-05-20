c5b406f35b913f52b404c320993a3d7dad3d05ff
shinpei maruyama <shinpeim@gmail.com>
2017-05-21 02:12:12 +0900

    refactor

--- src/main/scala/com/nekogata/scalajs_todo/js_bridge/MakeTodoDoneCommand.scala
+++ src/main/scala/com/nekogata/scalajs_todo/js_bridge/MakeTodoDoneCommand.scala
@@ -1,12 +1,13 @@
-package com.nekogata.scalajs_todo.usecase.command.make_todo_done
+package com.nekogata.scalajs_todo.js_bridge
 
 import com.nekogata.scalajs_todo.domain.TodoRepository
 import com.nekogata.scalajs_todo.infrastracture.InMemoryTodoRepository
+import com.nekogata.scalajs_todo.usecase.command.make_todo_done.Command
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("MakeTodoDoneCommand")
-class CommandImpl extends Command{
+class MakeTodoDoneCommand extends Command{
   override val repository: TodoRepository = new InMemoryTodoRepository
 
   @JSExport
